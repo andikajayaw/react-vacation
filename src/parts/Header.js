1ec4ece08d91a64b9a7e19740024f7f63a0a0b35
andikajayaw <andika@sindata.net>
2024-11-08 16:51:30 +0800

    feat: sample header

---
+++ src/parts/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Button from '../Button'
+export default function Header(props) {
+    return (
+        <Header className="spacing-sm">
+            <div className='container'>
+                
+            </div>
+        </Header>
+    )
+}
