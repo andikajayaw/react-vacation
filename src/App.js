3d962e69c476715c74f2d073c671f993bec80702
andikajayaw <andika@sindata.net>
2024-11-12 17:00:16 +0800

    feat: styling header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LandingPage from "./pages/LandingPage"
+
 import './assets/scss/style.scss'
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Router>
+        <Routes>
+          <Route path='/' Component={LandingPage}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
--- src/parts/IconText.js
+++ src/parts/IconText.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Button from "../Button/index"
+import Button from "../elements/Button"
 export default function IconText() {
   return (
     <Button className="brand-text-icon" href="" type="link">
